4dd9faa719a03a4b48931f1ab1f757b57c94b2f7
Mario-aj <mariojorge1997@mail.ru>
2021-09-16 06:57:25 +0100

    add tailwindcss to project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-   <h1>Ola! welcome to betfight.</h1>
+   <h1 className="text-xl text-red-500 font-bold">Welcome to betfight.</h1>
   )
 }
 
