745f85c2d17c7dd2dfe4d6ded5e2afea505e1d5e
mario-aj <mariojorge1997@mail.ru>
2022-01-12 18:07:33 +0100

    chore: config tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>Hello, world!</h1>
+      <h1 className="text-3xl font-bold underline text-blue-500 cursor-pointer">
+        Hello, world!
+      </h1>
     </div>
   );
 }
