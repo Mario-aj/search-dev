b015b9247ef48f7b43d240a0f00d06678b0e2446
mario-aj <mariojorge1997@mail.ru>
2022-01-13 11:54:25 +0100

    improve custom hook, to save gh-code into localStorage

--- src/hooks/useGhCode.ts
+++ src/hooks/useGhCode.ts
@@ -3,10 +3,21 @@ import { useLayoutEffect, useState } from 'react';
 const useGhCode = () => {
   const [hasCode, setHasCode] = useState(false);
 
+  function getCodeFromUrl() {
+    let code = window.location.href.split('=')[1];
+
+    if (code) {
+      localStorage.setItem('gh-dev-code', JSON.stringify(code));
+      return code;
+    }
+
+    return '';
+  }
+
   useLayoutEffect(() => {
     let code = window.localStorage.getItem('gh-dev-code');
 
-    code = code ? JSON.parse(code) : window.location.href.split('=')[1];
+    code = code ? JSON.parse(code) : getCodeFromUrl();
 
     setHasCode(!!code);
   }, []);
