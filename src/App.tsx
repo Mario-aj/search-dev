909147d45bcc32ccdb230446238cb976733f6e9b
mario-aj <mariojorge1997@mail.ru>
2022-01-14 21:48:39 +0100

    build private routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
-import Routers from './routes';
+import React from 'react';
+import {
+  BrowserRouter,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
-function App() {
-  return <Routers />;
-}
+import Home from './Home';
+import { Login } from './Login';
+
+const PrivateRoute = () => {
+  const isLoggedIn = localStorage.getItem('gh-dev-token') !== null;
+
+  return isLoggedIn ? <Outlet /> : <Navigate to="/login" />;
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <React.Fragment>
+        <Routes>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </React.Fragment>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/Login/Login.tsx
+++ src/Login/Login.tsx
@@ -8,24 +8,26 @@ const Login = () => {
   };
 
   return (
-    <div className="items-center justify-center w-full h-full gap-4 bg-gray-200 rounded-lg md:flex md:w-3/4 lg:w-3/5">
-      <LoginIcon />
-      <div className="flex flex-col items-center justify-center w-full h-full p-2 md:w-1/2">
-        <div className="flex flex-col items-center justify-center mb-8">
-          <h1 className="text-4xl font-bold text-gray-800 uppercase">
-            Welcome
-          </h1>
-          <p className="text-lg text-center text-gray-500">
-            Click on the button to login with your github
-          </p>
+    <div className="flex items-center justify-center w-full h-screen p-4 bg-gray-900 md:p-6">
+      <div className="items-center justify-center w-full h-full gap-4 bg-gray-200 rounded-lg md:flex md:w-3/4 lg:w-3/5">
+        <LoginIcon />
+        <div className="flex flex-col items-center justify-center w-full h-full p-2 md:w-1/2">
+          <div className="flex flex-col items-center justify-center mb-8">
+            <h1 className="text-4xl font-bold text-gray-800 uppercase">
+              Welcome
+            </h1>
+            <p className="text-lg text-center text-gray-500">
+              Click on the button to login with your github
+            </p>
+          </div>
+          <a
+            className="flex items-center justify-center gap-2 px-4 py-3 text-xl font-bold text-white uppercase transition-all duration-300 bg-indigo-500 rounded-md hover:bg-indigo-600 active:bg-indigo-700"
+            href={`https://github.com/login/oauth/authorize?scope=user&client_id=${gh.client_id}&redirect_uri=${gh.redirect_uri}`}
+          >
+            <FaGithub />
+            login with github
+          </a>
         </div>
-        <a
-          className="flex items-center justify-center gap-2 px-4 py-3 text-xl font-bold text-white uppercase transition-all duration-300 bg-indigo-500 rounded-md hover:bg-indigo-600 active:bg-indigo-700"
-          href={`https://github.com/login/oauth/authorize?scope=user&client_id=${gh.client_id}&redirect_uri=${gh.redirect_uri}`}
-        >
-          <FaGithub />
-          login with github
-        </a>
       </div>
     </div>
   );
