ae2fd8b646bf206765d852bb644885069b3a034d
mario-aj <mariojorge1997@mail.ru>
2022-01-14 20:07:46 +0100

    feat: make a new route structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import { useGhCode } from './hooks';
-import { Login } from './Login';
+import Routers from './routes';
 
 function App() {
-  const ghCode = useGhCode();
-
-  return ghCode ? (
-    <div>Welcome to home page :) !</div>
-  ) : (
-    <div className="flex items-center justify-center w-full h-screen p-4 bg-gray-900 md:p-6">
-      <Login />
-    </div>
-  );
+  return <Routers />;
 }
 
 export default App;
