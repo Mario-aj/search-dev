f6d307c9024fb547d61ce5381d278d90a36d6017
mario-aj <mariojorge1997@mail.ru>
2022-01-16 21:30:44 +0100

    test: remove unused code and write code for backend calls

--- src/Login/__tests__/Login.test.tsx
+++ src/Login/__tests__/Login.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Login from '../Login';
 
