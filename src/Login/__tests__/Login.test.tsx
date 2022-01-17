5c9a9885c209c4d6558f2ab39feb46ddc40f0ef3
mario-aj <mariojorge1997@mail.ru>
2022-01-17 17:15:02 +0100

    refactor: remove Login snapshots

--- src/Login/__tests__/Login.test.tsx
+++ src/Login/__tests__/Login.test.tsx
@@ -12,38 +12,19 @@ const MockLogin = () => {
 };
 
 describe('Login', () => {
-  describe('Behavior', () => {
-    it('should render correctly', () => {
-      render(<MockLogin />);
+  it('should render correctly', () => {
+    render(<MockLogin />);
 
-      expect(screen.getByTitle(/LoginIcon/i)).toBeInTheDocument();
-      expect(
-        screen.getByRole('heading', { name: /welcome/i })
-      ).toBeInTheDocument();
-      expect(
-        screen.getByText(/click on the button to login with your github/i)
-      ).toBeInTheDocument();
+    expect(screen.getByTitle(/LoginIcon/i)).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', { name: /welcome/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText(/click on the button to login with your github/i)
+    ).toBeInTheDocument();
 
-      const link = screen.getByRole('link', { name: /login with github/i });
-      expect(link).toBeInTheDocument();
-      expect(link.childNodes.length).toBe(2);
-    });
-  });
-
-  describe('Styles', () => {
-    it('matches snapshots', () => {
-      render(<MockLogin />);
-
-      expect(screen.getByTitle(/LoginIcon/i)).toMatchSnapshot();
-      expect(
-        screen.getByRole('heading', { name: /welcome/i })
-      ).toMatchSnapshot();
-      expect(
-        screen.getByText(/click on the button to login with your github/i)
-      ).toMatchSnapshot();
-      expect(
-        screen.getByRole('link', { name: /login with github/i })
-      ).toMatchSnapshot();
-    });
+    const link = screen.getByRole('link', { name: /login with github/i });
+    expect(link).toBeInTheDocument();
+    expect(link.childNodes.length).toBe(2);
   });
 });
