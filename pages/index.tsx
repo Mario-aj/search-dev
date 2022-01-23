20dbfe761e1e2906f3e158d53ff3e59d25b1d06e
mario-aj <mariojorge1997@mail.ru>
2022-01-23 09:08:47 +0100

    use user hooks to manage user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,36 +1,29 @@
 import * as React from 'react';
-import { useRouter } from 'next/router';
-import getUserInfos from 'app/services/getUserInfos';
-import { Dropdown } from 'app/components/ui';
-import PrivateRoutes from 'app/components/PrivateRoutes';
 
-type User =
-  | {
-      avatar_url: string;
-      name: string;
-    }
-  | undefined;
+import Header from 'app/components/header';
+import getUserInfos from 'app/services/getUserInfos';
+import { useUserInfos } from 'app/hooks/useUserInfos';
+import PrivateRoutes from 'app/private-routes';
 
 type Props = {
   token: string;
 };
 
 const Home = ({ token }: Props) => {
-  const router = useRouter();
-  const [isOpen, setIsOpen] = React.useState(false);
-  const [user, setUser] = React.useState<User>();
+  const { dispatchUser } = useUserInfos();
 
   React.useEffect(() => {
     const getUser = async () => {
       try {
         if (token) {
-          const reponse = await getUserInfos(token as string);
+          const reponse = await getUserInfos(token);
 
           const data = {
             avatar_url: reponse.avatar_url,
             name: reponse.name,
           };
-          setUser(data);
+
+          dispatchUser(data);
         }
       } catch (error) {
         console.log('home page', error);
@@ -38,40 +31,11 @@ const Home = ({ token }: Props) => {
     };
 
     getUser();
-  }, []);
-
-  function handleLogout() {
-    localStorage.removeItem('gh-dev-access_token');
-    router.push('/login');
-  }
-
-  if (!user) return null;
+  }, [token, dispatchUser]);
 
   return (
     <div className="w-full h-screen bg-slate-100">
-      <div className="flex flex-row items-center justify-center w-full h-20 px-8 bg-white border-b border-b-gray-200">
-        <div className="flex items-center justify-between w-full h-full max-w-5xl">
-          <h1 className="text-2xl font-bold text-gray-800">Gh-dev</h1>
-          <Dropdown
-            avatarUrl={user.avatar_url}
-            title={user.name}
-            open={isOpen}
-            onOpen={() => setIsOpen((open) => !open)}
-          >
-            <ul className="flex flex-col w-full gap-1 text-gray-800 hover:transition-colors hover:duration-300">
-              <li className="w-full px-2 py-1 cursor-pointer hover:bg-blue-100">
-                profile
-              </li>
-              <li
-                onClick={handleLogout}
-                className="w-full px-2 py-1 cursor-pointer hover:bg-blue-100"
-              >
-                logout
-              </li>
-            </ul>
-          </Dropdown>
-        </div>
-      </div>
+      <Header />
     </div>
   );
 };
